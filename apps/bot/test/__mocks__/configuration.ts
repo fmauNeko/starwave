40c8fb8da2bb54a8cc36ba13e0f2377c91e06dbd
Florian Maunier <fmauneko@dissidence.ovh>
2025-12-18 01:00:01 +0100

    test(bot): mock configuration

---
+++ apps/bot/test/__mocks__/configuration.ts
@@ -0,0 +1,19 @@
+import { type } from 'arktype';
+import fs from 'node:fs';
+import path from 'node:path';
+import { JsonConfig, configSchema } from '../../src/config/config.type';
+
+export default (): JsonConfig => {
+  const jsonFileContent = fs.readFileSync(
+    path.join(__dirname, '..', 'config.test.json'),
+    'utf-8',
+  );
+
+  const validationResult = configSchema.json(JSON.parse(jsonFileContent));
+
+  if (validationResult instanceof type.errors) {
+    throw validationResult.toTraversalError();
+  }
+
+  return validationResult;
+};
--- apps/bot/test/app.e2e-spec.ts
+++ apps/bot/test/app.e2e-spec.ts
@@ -2,14 +2,17 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { App } from 'supertest/types';
+import { vi } from 'vitest';
 import { DiscordModule } from '../src/discord/discord.module';
 import { AppModule } from './../src/app.module';
-import { DiscordMockModule } from './mocks/discord.mock.module';
+import { DiscordMockModule } from './__mocks__/discord.mock.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
+    vi.mock(import('../src/config/configuration.js'));
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
